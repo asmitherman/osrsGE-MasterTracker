afc9b6b286f19f7bdd5df50dbd96fd207b45862f
Austin Smitherman <asmitherman@mail.csuchico.edu>
2018-12-06 17:02:41 -0600

    Updates

---
+++ precache-manifest.5cd56ba90b9847c7582ab7926ed32194.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/osrsGE-MasterTracker/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "b6233e3e97e5f9e974d6",
+    "url": "/osrsGE-MasterTracker/static/css/main.1854ae27.chunk.css"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/osrsGE-MasterTracker/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "7c1507006a1a90f20d3d",
+    "url": "/osrsGE-MasterTracker/static/js/1.7c150700.chunk.js"
+  },
+  {
+    "revision": "785d52b50b94e64ecda1",
+    "url": "/osrsGE-MasterTracker/static/js/runtime~main.785d52b5.js"
+  },
+  {
+    "revision": "031b13541693af2b22d25de5561124bc",
+    "url": "/osrsGE-MasterTracker/static/media/react-logo.031b1354.png"
+  },
+  {
+    "revision": "b6233e3e97e5f9e974d6",
+    "url": "/osrsGE-MasterTracker/static/js/main.b6233e3e.chunk.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/osrsGE-MasterTracker/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/osrsGE-MasterTracker/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/osrsGE-MasterTracker/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/osrsGE-MasterTracker/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "7c1507006a1a90f20d3d",
+    "url": "/osrsGE-MasterTracker/static/css/1.cd2eb447.chunk.css"
+  },
+  {
+    "revision": "2789485e5e90a1ad134b7b9589df5760",
+    "url": "/osrsGE-MasterTracker/index.html"
+  }
+];
