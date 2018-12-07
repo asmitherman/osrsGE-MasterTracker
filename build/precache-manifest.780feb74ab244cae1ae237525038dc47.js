a78cee985e6f834321a347655bc0b305f3dd32c9
Austin Smitherman <asmitherman@mail.csuchico.edu>
2018-12-06 20:24:01 -0600

    adding prod build with express server

---
+++ build/precache-manifest.780feb74ab244cae1ae237525038dc47.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "./static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "a4ee6b37a444447e4f5f",
+    "url": "./static/css/main.d2b9de82.chunk.css"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "./static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "dda620400de1b9a7d3d4",
+    "url": "./static/js/1.dda62040.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "031b13541693af2b22d25de5561124bc",
+    "url": "./static/media/react-logo.031b1354.png"
+  },
+  {
+    "revision": "a4ee6b37a444447e4f5f",
+    "url": "./static/js/main.a4ee6b37.chunk.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "./static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "./static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "./static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "./static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "dda620400de1b9a7d3d4",
+    "url": "./static/css/1.cd2eb447.chunk.css"
+  },
+  {
+    "revision": "2ff3178a6c796a108db0bc53f0fe65cd",
+    "url": "./index.html"
+  }
+];
