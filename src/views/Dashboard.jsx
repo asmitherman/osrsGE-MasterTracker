8dd298b25cb47531a0982952e3c6ad86f0d47ca2
Austin Smitherman <asmitherman@mail.csuchico.edu>
2018-12-13 21:08:21 -0600

    sample twitter posts in dashboard

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -131,9 +131,31 @@ class Dashboard extends React.Component {
       </Card>
       </Col>
       <Col lg="3" md="12">
-      <Card>
+      <Card className="border border-info">
       <CardBody>
-      News & Game Updates
+      <div className="dashTitle">
+        <i className="fas fa-exchange-alt"></i>
+        &nbsp;
+        &nbsp;
+        &nbsp;
+        <b>News & Game Updates</b>
+
+        </div>
+
+
+      </CardBody>
+      </Card>
+      <Card className="border border-info">
+      <CardBody>
+      <blockquote class="twitter-tweet" data-cards="hidden" data-lang="en"><p lang="en" dir="ltr">It&#39;s time for the global release of <a href="https://twitter.com/hashtag/OSRSMobile?src=hash&amp;ref_src=twsrc%5Etfw">#OSRSMobile</a>!<a href="https://t.co/FcvyNRoqMJ">https://t.co/FcvyNRoqMJ</a><br/><br/>(If on a mobile device you’ll go to the App Store or Google Play Store. If you’re on desktop you’ll go to our launch site)<br/><br/>...and yes, Winter 2017 has been the longest Winter yet 🥶 <a href="https://t.co/fgTZXxOvgw">pic.twitter.com/fgTZXxOvgw</a></p>&mdash; Old School RuneScape (@OldSchoolRS) <a href="https://twitter.com/OldSchoolRS/status/1057224510926143489?ref_src=twsrc%5Etfw">October 30, 2018</a></blockquote>
+      <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+      </CardBody>
+      </Card>
+      <Card className="border border-info">
+      <CardBody>
+      <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">It&#39;s time to get festive at the Old School Christmas Event of 2018 - now live with today&#39;s update! See all the details here: <a href="https://t.co/8Z86FuciCA">https://t.co/8Z86FuciCA</a></p>&mdash; Old School RuneScape (@OldSchoolRS) <a href="https://twitter.com/OldSchoolRS/status/1073179476232757250?ref_src=twsrc%5Etfw">December 13, 2018</a></blockquote>
+      <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
       </CardBody>
       </Card>
       </Col>
